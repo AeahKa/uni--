b07b08e116ba1853a84a1fd6991d84309a80851f
AeahKa <aeah@qq.com>
2023-02-22 18:13:29 +0800

    完成基础功能，能随机选择内置项

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,27 +1,32 @@
 <template>
 	<view class="content">
 		<view class="text-area">
-			<text class="main">今天吃{{item}}</text>
+			<text class="main">去吃{{item}}</text>
 		</view>
 		<view class="operator">
-			<button class="btn" style="color: #222222;">{{buttonText}}</button>
+			<button class="btn" style="color: #222222;" @click="start">{{choosing?'换一个':'随机选一样'}}</button>
 		</view>
 	</view>
 </template>
 
 <script>
+	import indexVue from './index.vue';
 	export default {
 		data() {
 			return {
 				item: '啥',
-				buttonText: "随机选一样"
+				list: ['白粥', '烧烤', '火锅', '猪脚饭', '沙拉', '肯德基', '麦当劳', '出门看到的第一家店', '饭堂'],
+				choosing: false
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			start() {
+				this.choosing = true
+				const total = this.list.length; //数据长度
+				const randomIndex = Math.floor(Math.random() * (total - 0))
+				this.item = this.list[randomIndex]
+				console.log(randomIndex)
+			}
 		}
 	}
 </script>
@@ -32,26 +37,25 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		padding: 16px;
 	}
 
 	.text-area {
 		display: flex;
 		justify-content: center;
-		border: red 1px solid;
 		margin-top: 20vh;
 		font-size: 72rpx;
 		font-weight: 600;
+		color: #222222;
 	}
 
 	.operator {
-		border: red 1px solid;
 		margin-top: 30vh;
 	}
 
 	.btn {
 		background: #ffd103;
 		font-size: 48rpx;
-		color: #8f8f94;
 		border-radius: 25cm 25cm 25cm 25cm;
 		width: 600rpx;
 	}
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -4,18 +4,26 @@ const _sfc_main = {
   data() {
     return {
       item: "\u5565",
-      buttonText: "\u968F\u673A\u9009\u4E00\u6837"
+      list: ["\u767D\u7CA5", "\u70E7\u70E4", "\u706B\u9505", "\u732A\u811A\u996D", "\u6C99\u62C9", "\u80AF\u5FB7\u57FA", "\u9EA6\u5F53\u52B3", "\u51FA\u95E8\u770B\u5230\u7684\u7B2C\u4E00\u5BB6\u5E97", "\u996D\u5802"],
+      choosing: false
     };
   },
-  onLoad() {
-  },
-  methods: {}
+  methods: {
+    start() {
+      this.choosing = true;
+      const total = this.list.length;
+      const randomIndex = Math.floor(Math.random() * (total - 0));
+      this.item = this.list[randomIndex];
+      console.log(randomIndex);
+    }
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
     a: common_vendor.t($data.item),
-    b: common_vendor.t($data.buttonText)
+    b: common_vendor.t($data.choosing ? "\u6362\u4E00\u4E2A" : "\u968F\u673A\u9009\u4E00\u6837"),
+    c: common_vendor.o((...args) => $options.start && $options.start(...args))
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/111/Documents/HBuilderProjects/\u4ECA\u5929\u5403\u5565/pages/index/index.vue"]]);
-wx.createPage(MiniProgramPage);
+const Component = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/111/Documents/HBuilderProjects/\u4ECA\u5929\u5403\u5565/pages/index/index.vue"]]);
+wx.createPage(Component);
